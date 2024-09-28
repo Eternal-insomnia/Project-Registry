5fa5010fe6bb12e9c86f6fa43a5594785d12e106
Лёхич <alexsuvorov2506@gmail.com>
2024-09-28 15:27:51 +0300

    font init commit

---
+++ frontend/CodeSoglasiyaFrontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- change logo -->
+  </header>
+  <main>
+    <p>GOAL</p>
+  </main>
+</template>
+
+<style scoped>
+  p {
+    color: blue;
+  }
+</style>
